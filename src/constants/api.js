9ffe3eeb1f77d18450436bffa877abc997ba6c99
Sabina <sabina@unitdtechnologie.com>
2023-12-21 18:33:16 +0530

    api

--- src/constants/api.js
+++ src/constants/api.js
@@ -5,17 +5,12 @@ let baseURL;
 if (process.env.NODE_ENV === 'production') {
   baseURL = 'http://43.228.126.245:3007';
 } else {
-  baseURL = 'http://43.228.126.245:3007';
+  baseURL = 'http://localhost:3007';
 }
 
 console.log('NODE_ENV:', process.env.NODE_ENV);
-// const api = axios.create({
-// <<<<<<< HEAD
-// //baseURL: 'http://43.228.126.245:3007',
-//  baseURL: 'http://localhost:3007',
-// =======
-//   baseURL,
-// >>>>>>> 76227c53075ce32562c245f02a3b9d93c548bac6
-// });
+const api = axios.create({
+  baseURL, // Use the baseURL variable here
+});
 
 export default api;

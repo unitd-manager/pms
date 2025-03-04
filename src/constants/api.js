d503f7c224561186e6fe9591b6f212270b8dc0f4
mafmeera <meera@unitdtechnologies.com>
2025-03-04 16:18:10 +0530

    first commit

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,38 +1,23 @@
-// import axios from 'axios';
-// import dotenv from 'dotenv';
+import axios from 'axios';
+import dotenv from 'dotenv';
 
 
-// dotenv.config();
-// let baseURL;
+dotenv.config();
+let baseURL;
 
-// const { hostname } = window.location;
+const { hostname } = window.location;
 
-// if (hostname === 'pmsdemo.unitdtechnologies.com') {
-//   baseURL = process.env.REACT_APP_PRODUCTION_URL;
-// } else if (hostname === 'pmsuts.unitdtechnologies.com') { 
-//   baseURL = process.env.REACT_APP_TEST_URL;
-// } else {
-//   baseURL = process.env.REACT_APP_LOCAL_URL;
-// }
+if (hostname === 'pmsdemo.unitdtechnologies.com') {
+  baseURL = process.env.REACT_APP_PRODUCTION_URL;
+} else if (hostname === 'pmsuts.unitdtechnologies.com') { 
+  baseURL = process.env.REACT_APP_TEST_URL;
+} else {
+  baseURL = process.env.REACT_APP_LOCAL_URL;
+}
 
-// console.log('Current Hostname:', hostname);
-// const api = axios.create({
-//   baseURL,
-// });
-
-// export default api; 
-import axios from 'axios'
-
+console.log('Current Hostname:', hostname);
 const api = axios.create({
-//baseURL:'http://66.29.149.122:3001',
-baseURL: 'https://pmsuts.unitdtechnologies.com:3008',
-//baseURL: 'http://localhost:3001',
+  baseURL,
 });
 
-
-// const loginApi = axios.create({
-//   baseURL: 'https://art-cause.com:3003'
-// });
-
-
-export default api
+export default api;
